f7927091e6d60fad1feed77e9bb470aeea3248b5
KrLiam <88288206+KrLiam@users.noreply.github.com>
2023-08-31 22:05:04 -0300

    implementa createProcess e changeState da ProcessTable

--- src/pcb.h
+++ src/pcb.h
@@ -12,4 +12,13 @@ struct PCB{
     ProcessState processState;
     int executingTime;
     Context context;
+
+    PCB(int id, int startTime, int duration, int priority)
+     : id(id),
+       startTime(startTime),
+       endTime(-1),
+       duration(duration),
+       priority(priority),
+       processState(pNew),
+       executingTime(0) {}
 };
--- src/table.h
+++ src/table.h
@@ -1,23 +1,55 @@
 #pragma once
 
 #include <vector>
+#include <unordered_map>
 
 #include "pcb.h"
 
 class ProcessTable {
     std::vector<PCB> processes;
 
+    std::unordered_map<ProcessState, std::vector<PCB*>> stateMap;
+
+    int currentId = 0;
+
+    int getNextId() {
+        return ++currentId;
+    }
+
+    std::vector<PCB*>& getCreateByState(ProcessState state) {
+        if (stateMap.count(state) <= 0) {
+            stateMap.emplace(state, std::vector<PCB*>());
+        }
+        return stateMap.at(state);
+    }
+
 public:
-    void createProcess(int creationTime, int duration, int priority);
+    void createProcess(int creationTime, int duration, int priority) {
+        PCB& process = processes.emplace_back(getNextId(), creationTime, duration, priority);
+        changeState(&process, pNew);
+    }
 
     PCB& getProcess(int id);
 
-    std::vector<PCB*>& getByState(ProcessState state);
+    const std::vector<PCB*>& getByState(ProcessState state);
 
-    void changeState(PCB* process, ProcessState newState) {
-        changeState(*process, newState);
+    void changeState(PCB* process, ProcessState state) {
+        // remove processo da lista do estado anterior
+        std::vector<PCB*>& prevState = getCreateByState(process->processState);
+        auto it = std::find(prevState.begin(), prevState.end(), process);
+        if (it != prevState.end()) {
+            prevState.erase(it);
+        }
+        // adiciona processo na lista do estado novo
+        std::vector<PCB*>& newState = getCreateByState(state);
+        auto newIt = std::find(newState.begin(), newState.end(), process);
+        if (newIt == newState.end()) {
+            newState.push_back(process);
+        }
+        
+        process->processState = state;
     }
-    void changeState(PCB& process, ProcessState newState);
+    void changeState(PCB& process, ProcessState state);
 
     void clear();
 };
