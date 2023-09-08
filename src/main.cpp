c6627a829e1e67d28d26351baabc8590de26affd
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2023-09-08 14:58:00 -0300

    Ordenar lista de criação dos processos

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@ int main()
 	File f;
 	f.read_file();
 	vector<ProcessParams*> all_processes = f.get_processes();
+	// sort the vector by arrival time
+	sort(all_processes.begin(), all_processes.end(), [](ProcessParams* a, ProcessParams* b) {
+		return a->creation_time < b->creation_time;
+		});
 	Scheduler scheduler = Scheduler();
 	vector<int> result = scheduler.simulate(all_processes);
 	scheduler.print_graph(result, 10, 4);
