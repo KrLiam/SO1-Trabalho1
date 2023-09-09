8d368951923ec8c941fed17ff5fad7f2ffb59f91
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2023-09-09 01:47:12 -0300

    Preparar para receber estrategia como argumento

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,34 @@
 #include "scheduler.h"
 #include "strategy.h"
 
-int main()
+int main(int argc, char const *argv[])
 {
+	if (argc != 2) {
+		std::cout << "Usage: ./main <strategy>" << std::endl;
+		return 1;
+	}
 	File f;
 	f.read_file();
 	std::vector<ProcessParams> all_processes = f.get_processes();
 
-	RoundRobin strategy;
-	Scheduler scheduler(strategy);
+	char strategy_name = argv[1][0];
+	
+	SchedulingStrategy* strategy;
+	switch (strategy_name)
+	{
+	case 'f':
+		strategy = new FCFS();
+		break;
+	case 'r':
+		strategy = new RoundRobin();
+		break;
+	default:
+		cout << "Invalid strategy" << endl;
+		return 1;
+	}
+
+	
+	Scheduler scheduler(*strategy);
 
 	std::vector<int> result = scheduler.simulate(all_processes);
 	scheduler.print_graph(result);
