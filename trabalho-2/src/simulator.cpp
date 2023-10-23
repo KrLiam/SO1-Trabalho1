408a5259cf851d917976a699ecd0b421855d5c37
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2023-10-22 21:11:45 -0300

    Comentários

--- trabalho-2/src/simulator.cpp
+++ trabalho-2/src/simulator.cpp
@@ -29,6 +29,7 @@ void Simulator::add_algorithm(SubstitutionAlgorithm& algorithm) {
 unsigned int Simulator::optimal(std::vector<page_t>& accesses) {
     int max_i = accesses.size();
 
+    // Mapa de posições de acesso para cada página
     std::unordered_map<page_t, std::list<int>> uses;
     for (int i = max_i - 1; i >= 0; i--) {
         page_t page = accesses[i];
@@ -45,14 +46,18 @@ unsigned int Simulator::optimal(std::vector<page_t>& accesses) {
     page_t furthest_page = -1;
     page_t furthest_page_i = -1;
 
+    // Inicia o cálculo
     for (std::size_t i = 0; i < accesses.size(); i++) {
         page_t page = accesses[i];
         std::list<int>& page_uses = uses.at(page);
 
+        // Salvar próxima página mais distante
         page_uses.pop_front();
         page_t page_next_i = page_uses.front();
 
         page_t prev_furthest_page = furthest_page;
+        // Caso a página acessada esteja mais distante que a página mais distante atual
+        // Atualizar a página mais distante atual
         if (page_next_i > furthest_page_i) {
             furthest_page_i = page_next_i;
             furthest_page = page;
@@ -77,8 +82,12 @@ unsigned int Simulator::optimal(std::vector<page_t>& accesses) {
         present_pages.erase(prev_furthest_page);
         present_pages.insert(page);
 
+        // Caso a página mais distante não tenha mudado e tenha ocorrido uma falha
+        // Encontrar a página mais distante
         if (prev_furthest_page == furthest_page) {
             furthest_page_i = -1;
+            // Busca a página das molduras com o maior valor de próximo acesso
+            // E atualiza a página mais distante atual
             for (page_t present_page : present_pages) {
                 std::list<int>& present_uses = uses.at(present_page);
                 page_t present_i = present_uses.front();
@@ -97,25 +106,24 @@ unsigned int Simulator::optimal(std::vector<page_t>& accesses) {
 SimulationResult Simulator::simulate(std::vector<page_t>& accesses, bool run_optimal) {
     std::vector<SimulationEntry> result_entries;
 
+    // Executar algoritmos adicionados ao simulador
     for (SubstitutionAlgorithm* algorithm : algorithms) {
         present_pages.clear();
         unsigned int faults = 0;
-
+        // Simular acessos para o algoritmo
         for (page_t page : accesses) {
+            // Página presente
             if (present_pages.count(page)) {
-                // std::cout << "page hit " << page << std::endl;
                 algorithm->accessed(page);
             }
+            // Página ausente
             else {
                 faults++;
-                // std::cout << "page fault " << page;
-
+                // Caso não haja molduras livres, pergunta ao algoritmo qual página deve ser removida para liberar espaço
                 if (present_pages.size() >= frame_amount) {
                     page_t removed_page = algorithm->remove();
                     present_pages.erase(removed_page);
-                    // std::cout << " removed page " << removed_page;
                 }
-                // std::cout << std::endl;
 
                 algorithm->insert(page);
                 present_pages.insert(page);
